16ab6ae0d414adfadde0c1ef8956f1b6c90f5b40
ignesnw <42905391+ignesnw@users.noreply.github.com>
2025-03-11 21:37:05 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/global.css';
-import '../styles/styles.module.css';
+import '../styles/styles.modules.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
